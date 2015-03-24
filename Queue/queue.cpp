54d609dac1ee75ddefb51286aa8a55f4c96c9c03
adnanaquib <aquib.adnan@gmail.com>
2015-03-24 14:13:55 -0400

    definitions done

--- Queue/queue.cpp
+++ Queue/queue.cpp
@@ -18,3 +18,46 @@ Queue::~Queue()
 		delete temp;
 	}
 }
+
+bool Queue::isempty() const
+{
+	return items;
+}
+
+bool Queue::enqueue(const Item &item)
+{
+	if (isfull())
+		return false;
+	Node * add = new Node;
+	if (add == NULL)
+		return false;
+	add->item = item;
+	add->next = NULL;
+	items++;
+	if (front == NULL)
+		front = add;
+	else
+		rear->next = add;
+	rear = add;
+	return true;
+}
+
+bool Queue::dequeue(Item & item)
+{
+	if (front == NULL)
+		return false;
+	item = front->item;
+	items--;
+	Node *temp = front;
+	front = front->next;
+	delete temp;
+	if (items == 0)
+		rear = NULL;
+	return true;
+}
+
+void Customer::set(long when)
+{
+	processtime = std::rand() % 3 + 1;
+	arrive = when;
+}
